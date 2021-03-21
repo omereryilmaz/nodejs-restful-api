44f69f0d0c4f86d0d6d6992845bae3346a0edcde
omereryilmaz <omer-eryilmaz@hotmail.com>
2021-03-21 21:29:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to NodeJS API!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
