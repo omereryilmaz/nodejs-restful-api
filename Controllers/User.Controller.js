b7cbf710f28c5a81b26ac39783e5439978d03469
omereryilmaz <omer-eryilmaz@hotmail.com>
2021-04-13 17:01:52 +0300

    New user method updated

--- Controllers/User.Controller.js
+++ Controllers/User.Controller.js
@@ -10,12 +10,19 @@ const User = require('../Models/User.Model');
 module.exports = {    
     createNewUser: async (req, res, next) => {
         try {
-            const user = new User(req.body);
-            user.password = await cryptAsync.hashStrSync(user.password);
-            const result = await user.save();
-            res.json({
-                message: 'User has been saved successfully!'
-            });
+            const result = await User.find({email: req.body.email});
+            
+            // If email already exist, don't save
+            if (result.length > 0) {
+                res.json({message: req.body.email + ' email already exist!'})
+            } else {
+                const user = new User(req.body);
+                user.password = await cryptAsync.hashStrSync(user.password);
+                const result = await user.save();
+                res.json({
+                    message: 'User has been saved successfully!'
+                });
+            }                   
         } catch (error) {
             console.log(error.message);
             next(error);
